e2bd72bac3e7823dca8c2c4eb0b39f2ae4b71e4f
Irena-St <semenchuk833@gmail.com>
2020-09-29 18:36:51 +0300

    Added wait-methods and asserts

--- src/tests/CurrentBoardTests.java
+++ src/tests/CurrentBoardTests.java
@@ -5,66 +5,118 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class CurrentBoardTests extends TestBase{
+public class CurrentBoardTests extends TestBase {
 
     @BeforeMethod
-    public void initTests() throws InterruptedException {
+    public void initTests() {
 
         WebElement loginIcon = driver.findElement(By
                 .xpath("//*[@class='btn btn-sm btn-link text-white']"));
         loginIcon.click();
-        Thread.sleep(5000);
+        //Thread.sleep(5000);
+        waitUntilElementIsClickable(By.id("user"),15);
         WebElement loginField = driver.findElement(By.id("user"));
         loginField.click();
         loginField.clear();
         loginField.sendKeys(LOGIN);
-        Thread.sleep(3000);
+        //Thread.sleep(3000);
+        waitUntilElementIsClickable(By.xpath("//input[@value='Log in with Atlassian']"),10);
         WebElement loginAttlButton = driver.findElement(By.id("login"));
         loginAttlButton.click();
-        Thread.sleep(3000);
+        //Thread.sleep(3000);
+        waitUntilElementIsClickable(By.id("password"),20);
         WebElement passAttlField = driver.findElement(By.id("password"));
         passAttlField.click();
         passAttlField.clear();
         passAttlField.sendKeys(PASSWORD);
+        waitUntilElementIsClickable(By.id("login-submit"),15);
         driver.findElement(By.id("login-submit")).click();
-        Thread.sleep(15000);
-        WebElement boardQAHaifa7 = driver.findElement(By.xpath("//div[@title='QA Haifa7']"));
+        //Thread.sleep(15000);
+        waitUntilElementIsClickable(By.xpath("//button[@data-test-id = 'header-boards-menu-button']"),45);
+        WebElement boardQAHaifa7 = driver.findElement(By.xpath("//li[@class='boards-page-board-section-list-item'][.//div[@title ='QA Haifa7']]"));
         boardQAHaifa7.click();
-        Thread.sleep(10000);
+        waitUntilElementIsClickable(By.id("workspaces-preamble-board-header-button"),15);
+        waitUntilElementIsPresent(By.tagName("h1"),10);
+
+    }
+    @Test
+    public void isCorrectCurrentBoard(){
+        System.out.println("Header of the current board: " +driver.findElement(By.tagName("h1")).getText());
+        Assert.assertEquals(driver.findElement(By.tagName("h1")).getText(),"QA Haifa7" ,
+                "The header of a screen is not 'QA Haifa7' ");
     }
 
     @Test
     public void CreateListPositive() throws InterruptedException {
 
+        waitUntilElementsAreVisible(By.xpath("//div[@class='list js-list-content']"),10);
         System.out.println("Lists quantity before: "+ driver
                 .findElements(By.xpath("//div[@class='list js-list-content']")).size());
+        int quantityListsInTheBegining = driver
+                .findElements(By.xpath("//div[@class = 'list js-list-content']")).size();
+        waitUntilElementIsClickable(By.className("placeholder"),5);
         WebElement addAListButton = driver.findElement(By.className("placeholder"));
         addAListButton.click();
+        waitUntilElementIsClickable(By.xpath("//input[@type='submit']"), 5);
         WebElement enterListTitle = driver.findElement(By.name("name"));
         enterListTitle.click();
+        enterListTitle.clear();
         enterListTitle.sendKeys("Test");
-        driver.findElement(By.xpath("//input[@class='primary mod-list-add-button js-save-edit']")).click();
-        Thread.sleep(5000);
+        WebElement submitNewList = driver.findElement(By.xpath("//input[@type='submit']"));
+        submitNewList.click();
+        waitUntilElementIsClickable(By.cssSelector("a.icon-close.dark-hover"), 5);
+        WebElement xButton = driver.findElement(By.cssSelector("a.icon-close.dark-hover"));
+        xButton.click();
+        waitUntilElementIsInvisible(By.cssSelector("a.icon-close.dark-hover"), 5);
+        int quantityListsAtTheEnd = driver
+                .findElements(By.xpath("//div[@class = 'list js-list-content']")).size();
         System.out.println("Lists quantity after: "+driver
                 .findElements(By.xpath("//div[@class='list js-list-content']")).size());
+        Assert.assertEquals(quantityListsAtTheEnd, quantityListsInTheBegining+1);
     }
 
     @Test
-    public void DeleteListTest() throws InterruptedException {
-        System.out.println("Lists quantity before: "+ driver
-                .findElements(By.xpath("//div[@class='list js-list-content']")).size());
-        WebElement listMenuIcon= driver.findElement(By.xpath("//div[@class=" +
-               "'board-main-content']//div[1]//div[1]//div[1]//div[2]//a[1]"));
-        listMenuIcon.click();
-        WebElement archiveThisList = driver.findElement(By.xpath("//a[@class='js-close-list']"));
-        archiveThisList.click();
-        Thread.sleep(5000);
+    public void putAnyListToArchive() throws InterruptedException {
+        //System.out.println("Lists quantity before: "+ driver
+              //  .findElements(By.xpath("//div[@class='list js-list-content']")).size());
+        WebElement addListButton = driver.findElement(By.cssSelector("a.open-add-list"));
+        if (addListButton.getText().equals("Add a list")) {
+            addListButton.click();
+            waitUntilElementIsClickable(By.xpath("//input[@type='submit']"), 5);
+            WebElement addNameListField = driver.findElement(By.xpath("//input[@name='name']"));
+            addNameListField.click();
+            addNameListField.clear();
+            addNameListField.sendKeys("test");
+            WebElement submitNewList = driver.findElement(By.xpath("//input[@type='submit']"));
+            submitNewList.click();
+            waitUntilElementIsClickable(By.cssSelector("a.icon-close.dark-hover"), 5);
+            WebElement xButton = driver.findElement(By.cssSelector("a.icon-close.dark-hover"));
+            xButton.click();
+            waitUntilElementIsInvisible(By.cssSelector("a.icon-close.dark-hover"), 5);
+        }
+        waitUntilElementsAreVisible(By.xpath("//div[@class='list js-list-content']"),10);
+            System.out.println("Lists quantity before: "+ driver
+                    .findElements(By.xpath("//div[@class='list js-list-content']")).size());
+            int quantityListsInTheBegining = driver
+                    .findElements(By.xpath("//div[@class = 'list js-list-content']")).size();
+            addListButton = driver.findElement(By.cssSelector("a.open-add-list"));
+        System.out.println("Text on the button: " +addListButton.getText());
+      WebElement extraMenu = driver.findElement(By.cssSelector("a.list-header-extras-menu"));
+      extraMenu.click();
+      waitUntilElementIsClickable(By.cssSelector("a.js-close-list"),15);
+      WebElement closeExtraMenu = driver.findElement(By.cssSelector("a.js-close-list"));
+      closeExtraMenu.click();
+      waitUntilElementIsInvisible(By.cssSelector("a.js-close-list"),5);
+      int quantityListsAtTheEnd = driver
+              .findElements(By.xpath("//div[@class = 'list js-list-content']")).size();
         System.out.println("Lists quantity after: "+driver
                 .findElements(By.xpath("//div[@class='list js-list-content']")).size());
+        Assert.assertEquals(quantityListsAtTheEnd, quantityListsInTheBegining-1);
     }
 
 }
--- src/tests/LoginTests.java
+++ src/tests/LoginTests.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -12,112 +15,110 @@ import org.testng.annotations.Test;
 public class LoginTests extends TestBase {
 
     @BeforeMethod
-    public void initTests() throws InterruptedException {
+    public void initTests() {
 
         WebElement loginIcon = driver.findElement(By
                 .xpath("//*[@class='btn btn-sm btn-link text-white']"));
         loginIcon.click();
-        Thread.sleep(10000);
+       // waitUntilElementIsVisible(By.id("password"),10);
+       // Thread.sleep(10000);
     }
+
+
     @Test
-    public void loginNegativeLoginEmpty() throws InterruptedException {
+    public void loginNegativeLoginEmpty()  {
         // Enter empty login and password
+        waitUntilElementIsClickable(By.id("password"),15);
         WebElement passwordField = driver.findElement(By.id("password"));
         passwordField.click();
         passwordField.clear();
         passwordField.sendKeys("marinaqa");
+        waitUntilElementIsClickable(By.id("login"),10);
         //Press login button
         WebElement loginButton = driver.findElement(By.id("login"));
         loginButton.click();
-        Thread.sleep(5000);
-        //Print error mssage
+        waitUntilElementIsPresent(By.id("error"), 10);
+        //.sleep(5000);
+        //Print error message
         System.out.println("Error: " + driver
                 .findElement(By.id("error")).getText());
+        Assert.assertEquals(driver.findElement(By.id("error")).getText(),"Missing email",
+                "The text of the error message is not correct");
     }
     @Test
-    public void loginNegativeTest() throws InterruptedException {
-
+    public void loginNegativeTest() {
+        waitUntilElementIsClickable(By.id("user"),10);
         WebElement enterEmail = driver.findElement((By.id("user")));
         enterEmail.click();
         enterEmail.clear();
         enterEmail.sendKeys("12345@");
+        waitUntilElementIsClickable(By.id("password"),10);
         WebElement passwordField = driver.findElement(By.id("password"));
         passwordField.click();
         passwordField.clear();
         passwordField.sendKeys("123123");
+        waitUntilElementIsClickable(By.id("login"),10);
         WebElement loginButton = driver.findElement(By.id("login"));
         loginButton.click();
-        Thread.sleep(5000);
+        waitUntilElementIsClickable(By.id("error"),20);
         System.out.println("Error: " + driver
                 .findElement(By.id("error")).getText());
+        Assert.assertEquals(driver.findElement(By.id("error")).getText(),"There isn't an account for this email",
+                "The text of the error message is not correct");
     }
     @Test
-    public void negativeTestWrongPass() throws InterruptedException {
-
+    public void negativeTestWrongPass() {
+        waitUntilElementIsClickable(By.id("user"),10);
         WebElement enterEmail = driver.findElement((By.id("user")));
         enterEmail.click();
         enterEmail.clear();
         enterEmail.sendKeys("semenchuk833@gmail.com");
-        Thread.sleep(3000);
+        waitUntilElementIsClickable(By.id("login"),10);
         WebElement atlassian = driver.findElement(By.id("login"));
         atlassian.click();
-        Thread.sleep(5000);
+        waitUntilElementIsClickable(By.id("password"),10);
         WebElement passwordField = driver.findElement(By.id("password"));
         passwordField.click();
         passwordField.clear();
         passwordField.sendKeys("123123123");
-        Thread.sleep(3000);
-        WebElement loginButton = driver.findElement(By.xpath("//button[@id='login-submit']//span[@class='css-t5emrf']"));
+        waitUntilElementIsClickable(By.xpath("//button[@id='login-submit']"),20);
+        WebElement loginButton = driver.findElement(By.xpath("//button[@id='login-submit']"));
         loginButton.click();
-        Thread.sleep(5000);
+        waitUntilElementIsClickable(By.xpath("//div[@id='login-error']"),15);
         System.out.println("Error: " + driver
-                .findElement(By.id("login-error")).getText());
+                .findElement(By.xpath("//div[@id='login-error']")).getText());
+         Assert.assertEquals(driver.findElement(By.xpath("//div[@id='login-error']")).getText(),
+                "Incorrect email address and/ or password.Do you need help logging in?");
     }
     @Test
-    public void positiveTest() throws InterruptedException {
+    public void positiveTest() {
 
+        waitUntilElementIsClickable(By.id("user"),10);
         WebElement enterEmail = driver.findElement((By.id("user")));
         enterEmail.click();
         enterEmail.clear();
-        enterEmail.sendKeys("semenchuk833@gmail.com");
-        Thread.sleep(2000);
+        enterEmail.sendKeys(LOGIN);
+        waitUntilElementIsClickable(By.xpath("//input[@value='Log in with Atlassian']"),10);
+        // Thread.sleep(2000);
         WebElement atlassian = driver.findElement(By.id("login"));
         atlassian.click();
+        waitUntilElementIsClickable(By.id("password"),15);
         WebElement passwordField = driver.findElement(By.id("password"));
         passwordField.click();
         passwordField.clear();
-        passwordField.sendKeys("123eva456bel");
-        Thread.sleep(2000);
+        passwordField.sendKeys(PASSWORD);
+        waitUntilElementIsClickable(By.id("login-submit"),15);
         WebElement loginButton = driver.findElement(By
                 .xpath("//button[@id='login-submit']//span[@class='css-t5emrf']"));
         loginButton.click();
-        Thread.sleep(30000);
-        System.out.println("Button text is: "+driver
+        //Thread.sleep(30000);
+        waitUntilElementIsClickable(By.xpath("//button[@data-test-id = 'header-boards-menu-button']"),45);
+       /* System.out.println("Button text is: "+driver
                 .findElement(By.xpath("//button[@data-test-id " +
-                        "= 'header-boards-menu-button']")).getText());
-        }
-    @Test
-    public void loginPositive() throws InterruptedException {
-        //Enter login field for Atlassian
-        WebElement loginField = driver.findElement(By.id("user"));
-        loginField.click();
-        loginField.clear();
-        loginField.sendKeys(LOGIN);
-        Thread.sleep(3000);
-        //Submit login Atlassian
-        WebElement loginAttlButton = driver.findElement(By.id("login"));
-        loginAttlButton.click();
-        Thread.sleep(15000);
-        //Enter Atlassian password and submit it
-        WebElement passAttlField = driver.findElement(By.id("password"));
-        passAttlField.click();
-        passAttlField.clear();
-        passAttlField.sendKeys(PASSWORD);
-        driver.findElement(By.id("login-submit")).click();
-        Thread.sleep(30000);
-        System.out.println("Boards button text: "
-                + driver.findElement(By.xpath("//button[@data-test-id " +
-                "= 'header-boards-menu-button']")).getText());
-    }
+                        "= 'header-boards-menu-button']")).getText());*/
+       Assert.assertTrue(driver.findElement(By.xpath(//button[@data-test-id \" +\n" +
+               "                        = 'header-boards-menu-button']")).getText().equals("Boards"),
+               "The text on the button is not correct");
 
+    }
 }
